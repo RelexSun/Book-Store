a301574be8a40bd6e0eec4a20335936b41c95e52
RelexSun <relexsunnop@gmail.com>
2023-09-21 17:05:43 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 // {// dont forget to install json-server from npm }
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const res = await axios.get("http://localhost:3001/books");
+
+    setBooks(res.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  // Don't do this: cuz it will request api over and over again
+  // fetchBooks();
+
+  const editBookById = async (id, newTitle) => {
+    const res = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...res.data };
       }
       return book;
     });
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const handleCreateBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.random(Math.random() * 9999), title },
-    ];
+  const handleCreateBook = async (title) => {
+    const res = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, res.data];
     setBooks(updatedBooks);
   };
 
